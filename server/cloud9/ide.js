208ed51c4bb4eeca8c07f4de8525804c64f4ddee
James Allen <jamesallen0108@gmail.com>
2011-08-11 07:05:09 +0100

    Enable LaTeX Compilation
    Include the LaTeX compiler plugin and CLSI proxy plugin which provide a
    compile button and PDF viewer

--- server/cloud9/ide.js
+++ server/cloud9/ide.js
@@ -122,7 +122,8 @@ Ide.DEFAULT_PLUGINS = [
     "ext/keybindings/keybindings",
     "ext/watcher/watcher",
     "ext/dragdrop/dragdrop",
-    "ext/beautify/beautify"
+    "ext/beautify/beautify",
+	"ext/latex/compile"
     //"ext/acebugs/acebugs"
 ];
 
